641ee1a5df5c60aa425a85f736a5818290bc940a
Andrey Sapeshko <andrdd17@gmail.com>
2025-05-24 08:41:43 +0300

    добавлена функция get_date

--- src/widget.py
+++ src/widget.py
@@ -7,3 +7,9 @@ def mask_account_card(type_card_number: str) -> str:
     else:
         from masks import get_mask_card_number
         return type_card_number[:-16] + get_mask_card_number(type_card_number[-16:])
+
+
+def get_date(date_long: str) -> str:
+    ''' возвращает строку с датой в формате "ДД.ММ.ГГГГ" '''
+
+    return date_long[8:10] + '.' + date_long[5:7] + '.' + date_long[:4]
