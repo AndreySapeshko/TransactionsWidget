97b45ce222f7343bae44c100f34d4a870ac58114
Andrey Sapeshko <andrdd17@gmail.com>
2025-05-28 17:11:44 +0300

    добавлен тест к функции get_mask_card_number

--- tests/test_masks.py
+++ tests/test_masks.py
@@ -7,10 +7,14 @@ from src.masks import get_mask_account, get_mask_card_number
 coll_account = [('64686473678894779589', '**9589'),
                ('35383033474447895560', '**5560')]
 
-coll_card = [('Maestro 1596837868705199', 'Maestro 1596 83** **** 5199'),
-            ('MasterCard 7158300734726758', 'MasterCard 7158 30** **** 6758'),
-            ('Visa Classic 6831982476737658', 'Visa Classic 6831 98** **** 7658')]
+coll_card = [('1596837868705199', '1596 83** **** 5199'),
+            ('7158300734726758', '7158 30** **** 6758'),
+            ('6831982476737658', '6831 98** **** 7658')]
 
 @pytest.mark.parametrize('account, expected', coll_account)
 def test_get_mask_account(account: str, expected: str):
     assert get_mask_account(account) == expected
+
+@pytest.mark.parametrize('card_number, expected', coll_card)
+def test_get_mask_card_number(card_number: str, expected: str):
+    assert get_mask_card_number(card_number) == expected
