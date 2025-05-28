0a04ac125b0a723c99a0bd3901cfcfc9ec431b12
Andrey Sapeshko <andrdd17@gmail.com>
2025-05-28 19:55:28 +0300

    добавил фикстуру в test_masks

--- tests/test_masks.py
+++ tests/test_masks.py
@@ -4,17 +4,26 @@ import pytest
 from src.masks import get_mask_account, get_mask_card_number
 
 
-coll_account = [('64686473678894779589', '**9589'),
-               ('35383033474447895560', '**5560')]
+@pytest.fixture(params=[
+    ('64686473678894779589', '**9589'),
+    ('35383033474447895560', '**5560')
+])
+def account_pair(request):
+    return request.param
+
+def test_account_masking(account_pair):
+    account, masked = account_pair
+    assert get_mask_account(account) == masked
 
 coll_card = [('1596837868705199', '1596 83** **** 5199'),
             ('7158300734726758', '7158 30** **** 6758'),
             ('6831982476737658', '6831 98** **** 7658')]
 
-@pytest.mark.parametrize('account, expected', coll_account)
-def test_get_mask_account(account: str, expected: str):
-    assert get_mask_account(account) == expected
-
 @pytest.mark.parametrize('card_number, expected', coll_card)
 def test_get_mask_card_number(card_number: str, expected: str):
     assert get_mask_card_number(card_number) == expected
+
+
+
+
+
