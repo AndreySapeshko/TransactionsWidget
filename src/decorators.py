4aa19dad356c8ed879caef9129261e489b3e7e25
Andrey Sapeshko <andrdd17@gmail.com>
2025-06-06 10:52:59 +0300

    исправления в соответствии с P8P и дополнил README

--- src/decorators.py
+++ src/decorators.py
@@ -1,16 +1,23 @@
 from datetime import datetime
 from functools import wraps
+from typing import Any, Callable, TypeVar
+
+T = TypeVar('T')
 
 
-def log(filename: str = ''):
-    def decorator(func):
+def log(filename: str = '') -> Callable[[Callable[..., T]], Callable[..., bool]]:
+    """ декоратор логирует разультаты работы функции и входные параметры
+        если передано имя и адрес файла, пишет в файл, если нет, в консоль """
+
+    def decorator(func: Callable[..., T]) -> Callable[..., bool]:
         @wraps(func)
-        def wrapper(*args, **kwargs):
+        def wrapper(*args: Any, **kwargs: Any) -> bool:
             try:
                 start_time = datetime.now()
                 result = func(*args, **kwargs)
                 finish_time = datetime.now()
-                message_log = f'{start_time} {finish_time} {func.__name__} ok. Result: {result}. Input: {args}, {kwargs}'
+                message_log = (f'{start_time} {finish_time} {func.__name__} ok. '
+                               f'Result: {result}. Input: {args}, {kwargs}')
             except Exception as e:
                 finish_time = datetime.now()
                 message_log = f'{start_time} {finish_time} erorr: {e.__context__}. Input: {args}, {kwargs}'
--- tests/test_decorators.py
+++ tests/test_decorators.py
@@ -3,11 +3,17 @@ import pytest
 from src.decorators import log
 
 
-@pytest.mark.parametrize('a, b, expected', [(5, 1, 'division ok. Result: 5.0. Input: (5, 1), {}\n'), (5, 0, 'erorr: None. Input: (5, 0), {}\n')])
-def test_log(a: int, b: int, expected: str, capsys):
+coll = [
+    (5, 1, 'division ok. Result: 5.0. Input: (5, 1), {}'),
+    (5, 0, 'erorr: None. Input: (5, 0), {}')
+]
+
+
+@pytest.mark.parametrize('a, b, expected', coll)
+def test_log(a: int, b: int, expected: str, capsys: pytest.CaptureFixture[str]) -> None:
     @log()
-    def division(a, b):
+    def division(a: int, b: int) -> float:
         return a / b
     division(a, b)
     captured = capsys.readouterr()
-    assert captured.out[54:] == expected
+    assert captured.out[54:] == expected + '\n'
