a4ae17c46f84af8200d3fc86960c004f9b835f5e
Andrey Sapeshko <andrdd17@gmail.com>
2025-06-06 09:22:12 +0300

    добавил модуль decorators c декоратором log

---
+++ src/decorators.py
@@ -0,0 +1,34 @@
+from datetime import datetime
+from functools import wraps
+
+
+def log(filename: str = ''):
+    def decorator(func):
+        @wraps(func)
+        def wrapper(*args, **kwargs):
+            try:
+                start_time = datetime.now()
+                result = func(*args, **kwargs)
+                finish_time = datetime.now()
+                message_log = f'{start_time} {finish_time} {func.__name__} ok. Result: {result}. Input: {args}, {kwargs}'
+            except Exception as e:
+                finish_time = datetime.now()
+                message_log = f'{start_time} {finish_time} erorr: {e.__context__}. Input: {args}, {kwargs}'
+            finally:
+                if filename == '':
+                    print(message_log)
+                else:
+                    with open(filename, 'a', encoding='utf-8') as file:
+                        file.write(message_log + '\n')
+            return True
+
+        return wrapper
+    return decorator
+
+
+@log('../data/mylog.txt')
+def some_func(a, b):
+    return a / b
+
+
+some_func(5, 0)
