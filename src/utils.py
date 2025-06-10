b4e1a46463adfedf512c0208d5a7591dbca7b505
Andrey Sapeshko <andrdd17@gmail.com>
2025-06-10 16:51:46 +0300

    внесены изменения в функции converter_from_json и test_converter_from_json

--- src/utils.py
+++ src/utils.py
@@ -5,10 +5,15 @@ import os
 def converter_from_json(filename: str) -> list[dict]:
     """ конвертирует json файл в python, если файла нет или пустой вернет пустой список """
 
-    result = []
+    json_data: list[dict] = []
     if os.path.exists(filename):
-        with open(filename, 'r', encoding='utf-8') as file:
-            data = json.load(file)
+        try:
+            with open(filename, 'r', encoding='utf-8') as file:
+                data = json.load(file)
+        except json.JSONDecodeError:
+            return json_data
+        except FileNotFoundError:
+            return json_data
         if type(data) is list and len(data) != 0:
-            result = data
-    return result
+            json_data = data
+    return json_data
