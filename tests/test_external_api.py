4fdf187afe72cd9f02996fd3cff213feb8776336
Andrey Sapeshko <andrdd17@gmail.com>
2025-06-10 06:54:52 +0300

    для функции get_transaction_amount_in_rubles тест изменен с использованием mock и patch

--- tests/test_external_api.py
+++ tests/test_external_api.py
@@ -2,6 +2,7 @@ import pytest
 
 
 from src.external_api import get_transaction_amount_in_rubles
+from unittest.mock import patch
 
 
 coll = [
@@ -39,11 +40,21 @@ coll = [
             "from": "MasterCard 7158300734726758",
             "to": "Счет 35383033474447895560"
         },
-        649483.001209
+        649488.123122
     )
 ]
 
 
+response = {
+    'success': True,
+    'query': {'from': 'USD', 'to': 'RUB', 'amount': 8221.37},
+    'info': {'timestamp': 1749526826, 'rate': 78.999987},
+    'date': '2025-06-10', 'result': 649488.123122
+}
+
+
 @pytest.mark.parametrize('transaction, expected', coll)
 def test_get_transaction_amount_in_rubles(transaction: dict, expected: float) -> None:
-    assert get_transaction_amount_in_rubles(transaction) == expected
+    with patch('requests.get') as mock_get:
+        mock_get.return_value.json.return_value = response
+        assert get_transaction_amount_in_rubles(transaction) == expected
