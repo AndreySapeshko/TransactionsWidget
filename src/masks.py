7edf94c122b24729d45ffcc992a3b80980f0d6cc
Andrey Sapeshko <andrdd17@gmail.com>
2025-06-13 09:49:40 +0300

    добавлено логирование в модуль masks.py

--- src/masks.py
+++ src/masks.py
@@ -1,7 +1,20 @@
+import logging
+
+
+logger = logging.getLogger(__name__)
+logger.setLevel(logging.INFO)
+file_handler = logging.FileHandler('logs/masks.log', 'w', encoding='utf-8')
+file_formater = logging.Formatter('%(asctime)s %(name)s %(levelname)s: %(message)s')
+file_handler.setFormatter(file_formater)
+logger.addHandler(file_handler)
+
+
 def get_mask_card_number(card_number: str) -> str:
     """ получаем номер карты и возвращаем со скрытими цифрами """
 
+    logger.info(f'функция get_mask_card_number запущена с аргументом: {card_number}')
     mask_card_number: str = card_number[:4] + ' ' + card_number[4:6] + "** **** " + card_number[-4:]
+    logger.info('функция get_mask_card_number успешно завершена')
 
     return mask_card_number
 
@@ -9,6 +22,8 @@ def get_mask_card_number(card_number: str) -> str:
 def get_mask_account(account: str) -> str:
     """ получаем номер счета возвращаем последние 4 цифры и две * """
 
+    logger.info(f'функция get_mask_account запущена с аргументом: {account}')
     mask_account: str = "".join("**" + account[-4:])
+    logger.info('функция get_mask_account успешно завершена')
 
     return mask_account
