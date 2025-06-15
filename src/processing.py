826f1441fcc65a6a206e288c866a545548df4cca
Andrey Sapeshko <andrdd17@gmail.com>
2025-06-15 10:27:46 +0300

    в модуль processing добавлена функция process_bank_search

--- src/processing.py
+++ src/processing.py
@@ -1,3 +1,6 @@
+import re
+
+
 def filter_by_state(list_of_dict: list, state: str = 'EXECUTED') -> list:
     """ выбираем из списка словорей по значению ключа "state" """
 
@@ -14,3 +17,13 @@ def sort_by_date(list_of_dict: list, reverse: bool = True) -> list:
     если reverse=False, то по возрастанию"""
 
     return sorted(list_of_dict, key=lambda x: x['date'], reverse=reverse)
+
+
+def process_bank_search(transactions:list[dict], search:str)->list[dict]:
+    """ из списка отбераем транзакции по заданному описанию """
+
+    result: list[dict] = []
+    for transaction in transactions:
+        if re.search(search, transaction['description']):
+            result.append(transaction)
+    return result
